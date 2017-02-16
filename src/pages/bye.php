02907cff359b337b2a9063a8b0ebfb86741f24b8
wangjh1 <wangjinhao@shopex.cn>
2017-02-16 19:18:03 +0800

    second

--- src/pages/bye.php
+++ src/pages/bye.php
@@ -6,10 +6,12 @@
  * Time: 13:13
  */
 
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 $request = Request::createFromGlobals();
-$response = new Response();
+
+$response = new Response('Goodbye');
+$response->send();
---
+++ src/pages/hello.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangjinhao
+ * Date: 2017/2/16
+ * Time: 19:13
+ */
